a6df979fc09f1d4c44c9dc6ba5fe80386c453dd6
2439905184 <2439905184@qq.com>
2026-06-09 18:10:34 +0800

    补充aigc并修复php代码

--- gtsimg.php
+++ gtsimg.php
@@ -1,7 +1,7 @@
 <?php
-const dir = "img/"
-const dir1 = "星际级巨大娘"
-$items = scandir(dir+dir1)
+define("dir","img/");
+define("dir1","星际级巨大娘");
+$items = scandir(dir.dir1);
 foreach($items as $item) {
     if($item == "." || $item == "..") continue;
     echo "<img src='".dir.dir1."/".$item."'/>";
